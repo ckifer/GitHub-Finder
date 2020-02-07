70c3a206f5522557eb0ec2053cc9b3931be2790a
Coltin R Kifer <Coltin.R.Kifer@ey.com>
2020-02-07 13:12:32 -0800

    finished app before refactor to use hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   };
 
   render() {
@@ -27,9 +31,10 @@ class User extends Component {
       following,
       public_repos,
       public_gists,
-      hireable
+      hireable,
+      company
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -54,9 +59,37 @@ class User extends Component {
             />
             <h1>{name}</h1>
             <p>Location: {location}</p>
-
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit GitHub Profile
+            </a>
+            <ul>
+              <li>
+                <strong>Username: </strong> {login}
+              </li>
+              <li>
+                <strong>Company: </strong> {company}
+              </li>
+              <li>
+                <strong>Website: </strong> {blog}
+              </li>
+            </ul>
           </div>
         </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
